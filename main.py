e3bc7a62496de6269d93d835fb97bb90cf963e9c
Jules <92825638+JulesHerrscher@users.noreply.github.com>
2023-03-28 10:32:22 +0200

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from src.methods.logistic_regression import LogisticRegression
 from src.methods.svm import SVM
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn
 
-
+# jules 
 def main(args):
     """
     The main function of the script. Do not hesitate to play with it
