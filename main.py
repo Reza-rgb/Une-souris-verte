7de304ecb35c54806747eb9d3e64182de7e250a5
Réza Machraoui <reza.machraoui@epfl.ch>
2023-04-05 19:15:35 +0200

    SVM main 1.1

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from src.methods.kmeans import KMeans
 from src.methods.logistic_regression import LogisticRegression
 from src.methods.svm import SVM
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn
- 
+
+
 def main(args):
     """
     The main function of the script. Do not hesitate to play with it
@@ -20,14 +21,12 @@ def main(args):
         args (Namespace): arguments that were parsed from the command line (see at the end 
                           of this file). Their value can be accessed as "args.argument".
     """
-    ## 1. First, we load our data and flatten the images into vectors
+    # 1. First, we load our data and flatten the images into vectors
     xtrain, xtest, ytrain, ytest = load_data(args.data)
     xtrain = xtrain.reshape(xtrain.shape[0], -1)
     xtest = xtest.reshape(xtest.shape[0], -1)
-    print("That's mine !")
 
-
-    ## 2. Then we must prepare it. This is were you can create a validation set,
+    # 2. Then we must prepare it. This is were you can create a validation set,
     #  normalize, add bias, etc.
 
     # Make a validation set (it can overwrite xtest, ytest)
@@ -35,15 +34,13 @@ def main(args):
         ### WRITE YOUR CODE HERE
         pass
     
-    ### WRITE YOUR CODE HERE to do any other data processing
-
+    # WRITE YOUR CODE HERE to do any other data processing
 
     # Dimensionality reduction (FOR MS2!)
     if args.use_pca:
         raise NotImplementedError("This will be useful for MS2.")
-    
 
-    ## 3. Initialize the method you want to use.
+    # 3. Initialize the method you want to use.
 
     # Use NN (FOR MS2!)
     if args.method == "nn":
@@ -51,13 +48,13 @@ def main(args):
     
     # Follow the "DummyClassifier" example for your methods
     if args.method == "dummy_classifier":
-        method_obj =  DummyClassifier(arg1=1, arg2=2)
+        method_obj = DummyClassifier(arg1=1, arg2=2)
 
-    elif ...:  ### WRITE YOUR CODE HERE
+    elif args.method == "svm":
+        method_obj = SVM(C=args.svm_c, kernel=args.svm_kernel, gamma=args.svm_gamma)
         pass
-    
 
-    ## 4. Train and evaluate the method
+    # 4. Train and evaluate the method
 
     # Fit (:=train) the method on the training data
     preds_train = method_obj.fit(xtrain, ytrain)
@@ -65,8 +62,7 @@ def main(args):
     # Predict on unseen data
     preds = method_obj.predict(xtest)
 
-
-    ## Report results: performance on train and valid/test sets
+    # Report results: performance on train and valid/test sets
     acc = accuracy_fn(preds_train, ytrain)
     macrof1 = macrof1_fn(preds_train, ytrain)
     print(f"\nTrain set: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
@@ -75,21 +71,27 @@ def main(args):
     macrof1 = macrof1_fn(preds, ytest)
     print(f"Test set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
 
-    ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
+    # WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
 
 
 if __name__ == '__main__':
     # Definition of the arguments that can be given through the command line (terminal).
     # If an argument is not given, it will take its default value as defined below.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', default="dataset_HASYv2", type=str, help="the path to wherever you put the data, if it's in the parent folder, you can use ../dataset_HASYv2")
-    parser.add_argument('--method', default="dummy_classifier", type=str, help="dummy_classifier / kmeans / logistic_regression / svm / nn (MS2)")
+    parser.add_argument('--data', default="dataset_HASYv2", type=str,
+                        help="the path to wherever you put the data, "
+                             "if it's in the parent folder, you can use ../dataset_HASYv2")
+    parser.add_argument('--method', default="dummy_classifier", type=str,
+                        help="dummy_classifier / kmeans / logistic_regression / svm / nn (MS2)")
     parser.add_argument('--K', type=int, default=10, help="number of clusters for K-Means")
     parser.add_argument('--lr', type=float, default=1e-5, help="learning rate for methods with learning rate")
     parser.add_argument('--max_iters', type=int, default=100, help="max iters for methods which are iterative")
-    parser.add_argument('--test', action="store_true", help="train on whole training data and evaluate on the test data, otherwise use a validation set")
+    parser.add_argument('--test', action="store_true",
+                        help="train on whole training data and evaluate on the test data, "
+                             "otherwise use a validation set")
     parser.add_argument('--svm_c', type=float, default=1., help="Constant C in SVM method")
-    parser.add_argument('--svm_kernel', default="linear", help="kernel in SVM method, can be 'linear' or 'rbf' or 'poly'(polynomial)")
+    parser.add_argument('--svm_kernel', default="linear",
+                        help="kernel in SVM method, can be 'linear' or 'rbf' or 'poly'(polynomial)")
     parser.add_argument('--svm_gamma', type=float, default=1., help="gamma prameter in rbf/polynomial SVM method")
     parser.add_argument('--svm_degree', type=int, default=1, help="degree in polynomial SVM method")
     parser.add_argument('--svm_coef0', type=float, default=0., help="coef0 in polynomial SVM method")
