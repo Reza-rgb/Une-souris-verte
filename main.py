c3d8fd83812df842cf5544064256f991c1fa1f99
Réza Machraoui <reza.machraoui@epfl.ch>
2023-04-18 10:27:50 +0200

    main updated as branch main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-import numpy as np 
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
@@ -16,53 +16,63 @@ def main(args):
     """
     The main function of the script. Do not hesitate to play with it
     and add your own code, visualization, prints, etc!
-
     Arguments:
-        args (Namespace): arguments that were parsed from the command line (see at the end 
+        args (Namespace): arguments that were parsed from the command line (see at the end
                           of this file). Their value can be accessed as "args.argument".
     """
-    # 1. First, we load our data and flatten the images into vectors
+    ## 1. First, we load our data and flatten the images into vectors
     xtrain, xtest, ytrain, ytest = load_data(args.data)
     xtrain = xtrain.reshape(xtrain.shape[0], -1)
     xtest = xtest.reshape(xtest.shape[0], -1)
 
-    # 2. Then we must prepare it. This is were you can create a validation set,
+    ## 2. Then we must prepare it. This is were you can create a validation set,
     #  normalize, add bias, etc.
+
+    # TODO: Normalization of the data
+
+    # Suffle of the training data
+    indices = np.random.permutation(xtrain.shape[0])
+    xtrain, ytrain = xtrain[indices, :], ytrain[indices]
 
     # Make a validation set (it can overwrite xtest, ytest)
     if not args.test:
-        ### WRITE YOUR CODE HERE
-        pass
-    
-    # WRITE YOUR CODE HERE to do any other data processing
+        ratio = 0.8
+        N = xtrain.shape[0]
+        limit = (int)(ratio * N)
+
+        xtrain, xvalid = xtrain[:limit, :], xtrain[limit:, :]
+        ytrain, yvalid = ytrain[:limit], ytrain[limit:]
 
     # Dimensionality reduction (FOR MS2!)
     if args.use_pca:
         raise NotImplementedError("This will be useful for MS2.")
 
-    # 3. Initialize the method you want to use.
+    ## 3. Initialize the method you want to use.
 
     # Use NN (FOR MS2!)
     if args.method == "nn":
         raise NotImplementedError("This will be useful for MS2.")
-    
+
     # Follow the "DummyClassifier" example for your methods
     if args.method == "dummy_classifier":
         method_obj = DummyClassifier(arg1=1, arg2=2)
+
+    elif args.method == "logistic_regression":
+        method_obj = LogisticRegression(lr=args.lr, max_iters=args.max_iters)
 
     elif args.method == "svm":
         method_obj = SVM(C=args.svm_c, kernel=args.svm_kernel, gamma=args.svm_gamma)
         pass
 
-    # 4. Train and evaluate the method
+    ## 4. Train and evaluate the method
 
     # Fit (:=train) the method on the training data
     preds_train = method_obj.fit(xtrain, ytrain)
-        
+
     # Predict on unseen data
     preds = method_obj.predict(xtest)
 
-    # Report results: performance on train and valid/test sets
+    ## Report results: performance on train and valid/test sets
     acc = accuracy_fn(preds_train, ytrain)
     macrof1 = macrof1_fn(preds_train, ytrain)
     print(f"\nTrain set: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
@@ -71,7 +81,7 @@ def main(args):
     macrof1 = macrof1_fn(preds, ytest)
     print(f"Test set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
 
-    # WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
+    ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
 
 
 if __name__ == '__main__':
@@ -79,16 +89,14 @@ if __name__ == '__main__':
     # If an argument is not given, it will take its default value as defined below.
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', default="dataset_HASYv2", type=str,
-                        help="the path to wherever you put the data, "
-                             "if it's in the parent folder, you can use ../dataset_HASYv2")
+                        help="the path to wherever you put the data, if it's in the parent folder, you can use ../dataset_HASYv2")
     parser.add_argument('--method', default="dummy_classifier", type=str,
                         help="dummy_classifier / kmeans / logistic_regression / svm / nn (MS2)")
     parser.add_argument('--K', type=int, default=10, help="number of clusters for K-Means")
     parser.add_argument('--lr', type=float, default=1e-5, help="learning rate for methods with learning rate")
     parser.add_argument('--max_iters', type=int, default=100, help="max iters for methods which are iterative")
     parser.add_argument('--test', action="store_true",
-                        help="train on whole training data and evaluate on the test data, "
-                             "otherwise use a validation set")
+                        help="train on whole training data and evaluate on the test data, otherwise use a validation set")
     parser.add_argument('--svm_c', type=float, default=1., help="Constant C in SVM method")
     parser.add_argument('--svm_kernel', default="linear",
                         help="kernel in SVM method, can be 'linear' or 'rbf' or 'poly'(polynomial)")
@@ -106,3 +114,5 @@ if __name__ == '__main__':
     # which can be accessed as "args.data", for example.
     args = parser.parse_args()
     main(args)
+
+# def cross_validation():
